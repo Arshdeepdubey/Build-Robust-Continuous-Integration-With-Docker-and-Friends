a3a7eea00549850b8fe9321658911af7d662b1f2
arshdeepdubey.ad <53818463+Arshdeepdubey@users.noreply.github.com>
2024-12-22 04:06:50 +0530

    Done CI/CD and e2e integration testing, made dockerfile changes. Changes concerning Docker .yml and Github action changes to be done later.

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,5 +1,6 @@
 # src/page_tracker/app.py
 
+import os
 from functools import cache
 
 from flask import Flask
@@ -16,4 +17,7 @@ def index():
 
 @cache
 def redis():
-    return Redis()
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
